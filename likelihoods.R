f8d7fb7f4a7b0cb94b8b0d2a15a212b7c6bc1d8a
alexey0308 <alexey.mipt@gmail.com>
2016-10-27 17:37:30 +0200

    defend environment of gene likelihood

--- likelihoods.R
+++ likelihoods.R
@@ -18,9 +18,30 @@ ll_gene <- function(counts, norm_factors, conditions,
             env <- c(as.list(params),param_list )
             mus <- eval(means_vector, env)
             lambdas <- mus[mean_indexes]
-            if(output){return(data.frame(lambdas=lambdas*norm_factors, count=counts))}
+            if(output){
+                return(data.frame(lambdas=lambdas*norm_factors, count=counts))
+            }
+            -sum(dpois(counts, (lambdas+1e-10)* norm_factors, log=TRUE))
+        }, parent.frame()
+    )
+    eval(funquote)
+}
+
+ll_shared_params <- function(shared_params, individual_gene_parameters, dat, forms){
+    #attach(dat)
+    param_names <- names(individual_gene_parameters)
+    mean_indexes <- sapply(conditions, match, names(forms))
+    means_vector<- makeVector(forms)
+    funquote <- substitute(
+        function(shared_params, individual_gene_parameters,output=FALSE){
+            names(params) <- param_names
+            env <- c(as.list(params),param_list )
+            mus <- eval(means_vector, env)
+            lambdas <- mus[mean_indexes]
+            if(output){
+                return(data.frame(lambdas=lambdas*norm_factors, count=counts))
+            }
             -sum(dpois(counts, (lambdas+1e-10)* norm_factors, log=TRUE))
         } 
     )
     eval(funquote)
-}
