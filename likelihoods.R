136afbae7fb426051f2e2d8de1e1f653c6ece831
alexey0308 <alexey.mipt@gmail.com>
2016-11-03 09:32:11 +0100

    prediction function added + fixed gene number in likelihood function

--- likelihoods.R
+++ likelihoods.R
@@ -48,6 +48,7 @@ ll_gene <- function(count_data, forms, param_names, shared_params=NULL){
     conditions <- count_data$condition
     counts <- count_data$count
     norm_factors <- count_data$norm_factor
+    stopifnot(!is.null(conditions),!is.null(norm_factors),!is.null(counts))
     mean_indexes <- sapply(conditions, match, names(forms))
     if(!is.null(shared_params)&&!anyNA(forms))
         forms <- lapply(forms, substitute_q, shared_params)
@@ -57,9 +58,6 @@ ll_gene <- function(count_data, forms, param_names, shared_params=NULL){
             names(params) <- param_names
             mus <- eval(means_vector, as.list(params))
             lambdas <- mus[mean_indexes]
-            if(output){
-                return(data.frame(lambdas=lambdas*norm_factors, count=counts))
-            }
             -sum(dpois(counts, (lambdas+1e-10)* norm_factors, log=TRUE))
         }, parent.frame()
     )
@@ -70,25 +68,51 @@ ll_gene <- function(count_data, forms, param_names, shared_params=NULL){
 ll_shared_params <- function(count_data,forms,individual_params,
                              shared_param_names){
     means_for_genes <- list()
+    gene_number <- length(individual_params$id)
     individual_params$id <- as.factor(individual_params$id)
     individual_params <- individual_params[order(individual_params$id),]
     forms_vector <- makeVector(forms)[[1]]
     condition_indexes <- sapply(count_data$condition, match, names(forms))
     means_indexes <- cbind(as.numeric(count_data$id), condition_indexes)
-    for(i in 1:2){
+    for(i in 1:gene_number){
         means_for_genes[[i]] <- substitute_q(
             forms_vector, env=individual_params[i,])
     }
     function(shared_params){
         names(shared_params ) <- shared_param_names
-        means_matrix <- matrix(0,ncol=length(forms), nrow=2)
-        for(i in 1:2){
+        means_matrix <- matrix(0,ncol=length(forms), nrow=gene_number)
+        for(i in seq_along(individual_params$id)){
             means_matrix[i,] <- eval(means_for_genes[[i]],
                                      as.list(shared_params))
         }
         lambdas <- means_matrix[means_indexes]
         -sum(dpois(count_data$count, (lambdas+1e-10), log=TRUE))
     }
+}
+
+predict.expression <- function(count_data, model, forms){
+    individual_params <- model$individual_params
+    shared_params <- model$shared_params
+    means_for_genes <- list()
+    
+    gene_number <- length(individual_params$id)
+    individual_params$id <- as.factor(individual_params$id)
+    individual_params <- individual_params[order(individual_params$id),]
+    forms_vector <- makeVector(forms)[[1]]
+    condition_indexes <- sapply(count_data$condition, match, names(forms))
+    means_indexes <- cbind(as.numeric(count_data$id), condition_indexes)
+    
+    for(i in 1:gene_number){
+        means_for_genes[[i]] <- substitute_q(
+            forms_vector, env=individual_params[i,])
+    }
+    means_matrix <- matrix(0,ncol=length(forms), nrow=gene_number)
+    for(i in seq_along(individual_params$id)){
+        means_matrix[i,] <- eval(means_for_genes[[i]],
+                                    as.list(shared_params))
+    }
+    lambdas <- means_matrix[means_indexes]
+    list(prediction=lambdas,logL=dpois(count_data$count, (lambdas+1e-10), log=TRUE))
 }
 
 
@@ -113,12 +137,12 @@ fitModel <- function(count_data,  formulas, individual_params,
     individual_params <- as.data.frame(do.call(rbind, new_params))
     names(individual_params) <- param_names
     individual_params$id <- rownames(individual_params)
-    individual_params
     # Fit shared params
     shared_objective <- ll_shared_params(count_data, formulas, 
         individual_params, shared_param_names)
     shared_params <- optim(rep(1,4), shared_objective, method="L-BFGS-B",
             lower=lower_boundary_shared, upper=upper_boundary_shared)$par
     names(shared_params) <- shared_param_names
+    
     list(individual_params=individual_params, shared_params=shared_params)
 }
