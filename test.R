8e63b8d0ca78ef5eb6044c16399853c4372ec40c
alexey0308 <alexey.mipt@gmail.com>
2016-11-23 16:35:17 +0100

    nb in individual and shared

--- test.R
+++ test.R
@@ -1,8 +1,13 @@
 #source("likelihoods.R") 
 
-generateTestData <- function(forms, individual_params, shared_params, n=1){
-   means <- sapply(forms, eval, c(as.list(individual_params), as.list(shared_params)))
-   counts <- rpois(n=length(means)*n, lambda=means)
+generateTestData <- function(forms,
+                             individual_params,
+                             shared_params,
+                             n=1,
+                             size=100){
+   means <- sapply(forms, eval,
+    c(as.list(individual_params), as.list(shared_params)))
+   counts <- rnbinom(n=length(means)*n, mu=means, size=size)
    data.frame(condition=rep(names(forms), n), count=counts)
 }
 
@@ -24,7 +29,7 @@ testIndividualGeneParams <- function(){
     d <- generateTestData(forms, p,alphas, n=2)
     d$norm_factor<- 1
 
-    likelihood <- ll_gene(d, forms, param_names=names(p), alphas )
+    likelihood <- ll_gene(d, forms, param_names=names(p), alphas, size=1e2)
 
     res <-optim(p, likelihood, method="L-BFGS-B", 
         lower=rep(1e-9, length(p)), upper=c(1e5,1e5, 1,1))
@@ -53,7 +58,7 @@ testSharedParams <- function(){
     }
     d <- do.call(rbind,d)
     d$norm_factor<- 1
-    f <- ll_shared_params (d, forms,p, names(alphas))
+    f <- ll_shared_params (d, forms,p, names(alphas), size=1e3)
     res <-optim(rep(1,4), f, method="L-BFGS-B", 
         lower=rep(1e-9, length(alphas)), upper=c(15,15, 4,4))
     res <- cbind(correct=unlist(alphas), estimated=res$par)
@@ -77,19 +82,21 @@ testFitModel <- function(){
                     mu_h=c(50,500), 
                      a_n=c(.5, .8),
                      a_h=c(.8, .5))
+    param_names <- names(p)
     alphas <- list(alpha_chase=2, alpha_lab=1.5, beta_chase=1,beta_lab=.8)
-    p$id <- c("a", "b")
+    p$id <- c("b","a")
     for(i in seq_along(p$id)){
-        d[[i]] <- cbind(id=p$id[i],generateTestData(forms, p[i,1:4],alphas, n=2))
+        d[[i]] <- cbind(id=p$id[i],generateTestData(forms, p[i,param_names],alphas, n=2))
     }
     d <- do.call(rbind,d)
     d$norm_factor<- 1
+    d <- d[ sample(length(d$id)),]
     # Fit
     options <- list(
         lower_boundary = rep(1e-9,4),
         upper_boundary = c(1e5,1e5,1,1)-1e-1,
         lower_boundary_shared = rep(1e-9,4),
-        upper_boundary_shared = rep(5)
+        upper_boundary_shared = rep(5,4)
     )
     individual_params <- as.data.frame(p[,-5])
     individual_params[,] <- .1
