2d3f9030ab04fb77582a8cf2656c7e8548065880
Alexey U <alexey.mipt@gmail.com>
2016-12-03 18:52:35 +0100

    animation code

--- logo.R
+++ logo.R
@@ -37,18 +37,31 @@ plotCircle <- function(){
 }
 
 
-plotDNA <- function(t,q0){
+plotDNA <- function(q0, t=1){
   n <- dim(DNA1)[1]
+  nas <- is.na(DNA1$y1)
   q1 <- q0 +
     geom_line(data=DNA1[1:round(t*n),],
-              aes(x=x, y=y1), color="green", alpha=.7, size=lineWidth) +
-    geom_line(data=DNA1[1:round(t*n),], aes(x=x, y=y2), color="green", alpha=.8, size=lineWidth)
+              aes(x=x, y=y2), color="green", alpha=.7, size=lineWidth) +
+    geom_line(data=DNA1[!nas & 1:n < round(t*n),],
+              aes(x=x, y=y1), color="green", alpha=.8, size=lineWidth)
+  if(t < 1){
+    p <- DNA1[round(t*n),]
+    q1 <- AddPoint(q1, p$x, p$y1)
+    q1 <- AddPoint(q1, p$x, p$y2)
+  }
+  q1
 }
 
-plotPulse <- function(t, q1){
+plotPulse <- function(q1, t=1){
   n <- dim(pulse)[1]
   q2 <- q1 + geom_line(data=pulse[1:round(t*n),], aes(x=x,y=y),
                       color="green", alpha=.9, size=lineWidth)
+  if(t < 1){
+    p <- pulse[round(t*n),]
+    q2 <- AddPoint(q2, p$x, p$y)
+  }
+  q2
 }
 
 AddText <- function(q){
@@ -60,18 +73,19 @@ AddText <- function(q){
 }
 
 AddPoint <- function(q,x,y){
+  d <- data.frame(x=x,y=y)
   q +
-  geom_point(aes(x=x,y=y),
+  geom_point(data=d, aes(x=x,y=y),
                  color="green",
                  fill="green",
-                 size=9,
+                 size=15,
                  shape=21,
                  stroke=0,
                  alpha=.2) +
-  geom_point(aes(x=x,y=y),
+  geom_point(data=d, aes(x=x,y=y),
                  color="green",
-                 size=3) +
-  geom_point(aes(x=x,y=y),
+                 size=5) +
+  geom_point(data=d, aes(x=x,y=y),
                  color="white",
                  size=1)
 }
@@ -91,8 +105,30 @@ pulse$y <- pulse$y - pulse$y[1]
 DNA1 <- DNA(a=-6,b=0, y=0, size=3,n=2,phi0=phi0,s=.7,decay=.2)
 DNA1$x <- DNA1$x + shift
 
-
+## plain logo
 q <- plotCircle()
 q <- AddText(q)
-q <- plotDNA(.5,q)
+q <- plotDNA(q)
+q <- plotPulse(q)
 ggsave(filename="logo.png", plot=q3, height=7,width=7)
+
+## animation
+
+trace <- function(){
+  q <- plotCircle()
+  q <- AddText(q)
+  t <- seq(from=0, to=1, by=1/10)
+  lapply(t, function(x){
+     print(plotDNA(q,t=x))})
+  q <- plotDNA(q)
+
+  frac <- .3
+  t <- seq(from=0, to=frac, by=1/10)
+  lapply(t, function(x){
+     print(plotPulse(q,t=x))})
+  t <- seq(from=frac, to=1, by=1/10)
+  lapply(t, function(x){
+     print(plotPulse(q,t=x))})
+}
+
+saveGIF(trace(), interval=.02, movie.name="logo.gif")
