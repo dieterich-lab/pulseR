63e527da6c0fc563ffc9ef5dfad81b03347a4ad0
Alexey U <alexey.mipt@gmail.com>
2016-12-03 18:32:29 +0100

    animation

--- logo.R
+++ logo.R
@@ -1,12 +1,6 @@
 library(ggplot2)
 library(magrittr)
 
-lineWidth <- 2
-
-phi <- 2*pi * 1:101/100
-R <- 10
-circle <- data.frame(x=R*sin(phi), y=R*cos(phi))
-q0 <- ggplot() +  geom_polygon(data=circle, aes(x=x,y=y)) + theme_void()
 
 #q0 <- ggplot() +
 #  geom_polygon(aes(x=c(-R,R,R,-R), y=c(R,R,-R,-R)), fill="black", alpha=.8) +
@@ -16,7 +10,8 @@ q0 <- ggplot() +  geom_polygon(data=circle, aes(x=x,y=y)) + theme_void()
 
 f <- function(x,s,R, r=1) {
   x <- x/r
-  (exp(-(x+s/2)^2) - exp(-(x-s/2)^2))*R
+  res <- (exp(-(x+s/2)^2) - exp(-(x-s/2)^2)*.9)*R
+  res *R /max(res)
 }
 
 DNA <- function(a,b,y,size, n, phi0,s,decay){
@@ -32,28 +27,72 @@ DNA <- function(a,b,y,size, n, phi0,s,decay){
   data.frame(x=x,y1=y1,y2=y2)
 }
 
+
+## Ploting
+plotCircle <- function(){
+  phi <- 2*pi * 1:101/100
+  circle <- data.frame(x=R*sin(phi), y=R*cos(phi))
+  q0 <- ggplot() +  geom_polygon(data=circle, aes(x=x,y=y)) + theme_void()
+  q0
+}
+
+
+plotDNA <- function(t,q0){
+  n <- dim(DNA1)[1]
+  q1 <- q0 +
+    geom_line(data=DNA1[1:round(t*n),],
+              aes(x=x, y=y1), color="green", alpha=.7, size=lineWidth) +
+    geom_line(data=DNA1[1:round(t*n),], aes(x=x, y=y2), color="green", alpha=.8, size=lineWidth)
+}
+
+plotPulse <- function(t, q1){
+  n <- dim(pulse)[1]
+  q2 <- q1 + geom_line(data=pulse[1:round(t*n),], aes(x=x,y=y),
+                      color="green", alpha=.9, size=lineWidth)
+}
+
+AddText <- function(q){
+  q3 <-  q + geom_text(aes(x=4.3, y=1.0, label="pulseR"),
+                        color="white", size=28, family="Ubuntu Mono"
+                        , fontface="italic"
+                      )
+  q3
+}
+
+AddPoint <- function(q,x,y){
+  q +
+  geom_point(aes(x=x,y=y),
+                 color="green",
+                 fill="green",
+                 size=9,
+                 shape=21,
+                 stroke=0,
+                 alpha=.2) +
+  geom_point(aes(x=x,y=y),
+                 color="green",
+                 size=3) +
+  geom_point(aes(x=x,y=y),
+                 color="white",
+                 size=1)
+}
+
+lineWidth <- 2
+R <- 10
 shift <- -3.4
 phi0 <- -2.1
 
 shiftPulse <- -1.7
-pulse <- seq(shiftPulse,R - shift,.1) %>% data.frame(x=.,y=f(.,.5,2*R,.8))
+pulse <- seq(shiftPulse,R - shift,.1) %>% data.frame(x=.,y=f(.,.5,R*.9,.8))
 pulse$x <- pulse$x + shift - shiftPulse
 pulse <- pulse[ pulse$x < R,]
+pulse$y <- pulse$y - pulse$y[1]
 
 
-DNA1 <- DNA(a=-6,b=0, y=pulse$y[1]+.1,size=3,n=2,phi0=phi0,s=.7,decay=.2)
+DNA1 <- DNA(a=-6,b=0, y=0, size=3,n=2,phi0=phi0,s=.7,decay=.2)
 DNA1$x <- DNA1$x + shift
 
-q1 <- q0 +
-  geom_line(data=DNA1, aes(x=x, y=y1),color="green", alpha=.7, size=lineWidth) +
-  geom_line(data=DNA1, aes(x=x, y=y2), color="green", alpha=.8, size=lineWidth)
 
-q2 <- q1 + geom_line(data=pulse, aes(x=x,y=y),
-                     color="green", alpha=.9, size=lineWidth)
-
-q3 <-  q2 + geom_text(aes(x=4.3, y=1.5, label="pulseR"),
-                      color="white", size=25, family="NimbusSan"
-                      , fontface="italic"
-                     )
-q3
+q <- plotCircle()
+q <- AddText(q)
+q <- plotDNA(.5,q)
 ggsave(filename="logo.png", plot=q3, height=7,width=7)
