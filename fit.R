e8415763146c57b9e2000a82511bf94b2c024ade
alexey0308 <alexey.mipt@gmail.com>
2016-12-22 16:40:14 +0100

    update interface in fitting

--- fit.R
+++ fit.R
@@ -13,23 +13,14 @@ defaultParams <- function() {
  
 ## params are a data.frame with ids in rownames
 # order is the same as in parameters
-fitIndividualParameters <- function(pulseData,
-                                    old_params,
-                                    shared_params,
-                                    options,
-                                    size) {
-  param_names <- colnames(old_params)
-  objective <- ll_gene(
-    pulseData = pulseData,
-    param_names = param_names,
-    size = size,
-    shared_params = shared_params
-  )
+fitIndividualParameters <- function(pulseData, par, options) {
+  param_names <- colnames(par$individual_params)
+  objective <- ll_gene(pulseData, par)
   new_params <- list()
   new_params <- mclapply(
-    X = seq_len(dim(old_params)[1]),
+    X = seq_len(dim(par$individual_params)[1]),
     FUN = function(i) {
-      olds <- old_params[i,]
+      olds <- par$individual_params[i,]
       optim(
         olds,
         objective,
@@ -43,42 +34,25 @@ fitIndividualParameters <- function(pulseData,
     ,mc.cores = options$cores
   )
   new_params <- do.call(rbind, new_params)
-  rownames(new_params) <- rownames(old_params)
+  rownames(new_params) <- rownames(par$individual_params)
   as.data.frame(new_params)
 }
 
-fitSharedParameters <- function(old_shared_params,
-                                pulseData,
-                                individual_params,
-                                options,
-                                size) {
-  shared_objective <- ll_shared_params(
-    pulseData,
-    individual_params = individual_params,
-    shared_param_names =  names(old_shared_params),
-    size =  size
-  )
+fitSharedParameters <- function(pulseData, par, options) {
+  shared_objective <- ll_shared_params(pulseData, par)
   shared_params <- optim(
-    unlist(old_shared_params),
+    unlist(par$shared_params),
     shared_objective,
     method = "L-BFGS-B",
     lower = options$lower_boundary_shared,
     upper = options$upper_boundary_shared
   )$par
-  names(shared_params) <- names(old_shared_params)
+  names(shared_params) <- names(par$shared_params)
   as.list(shared_params)
 }
 
-fitDispersion <- function(shared_params,
-                          pulseData,
-                          individual_params,
-                          options,
-                          size) {
-  dispersion_objective <- ll_dispersion(
-    pulseData = pulseData,
-    individual_params =  individual_params,
-    shared_params =  shared_params
-  )
+fitDispersion <- function(pulseData, par, options) {
+  dispersion_objective <- ll_dispersion(pulseData, par)
   size <- optimise(dispersion_objective,
                    interval = unlist(options[c("lower_boundary_size",
                                                "upper_boundary_size")]))$minimum
@@ -90,17 +64,13 @@ getMaxRelDifference <- function(x,y) max(abs(1 - unlist(x)/unlist(y)))
 # options is a list with records
 # - individual_rel_err
 # - shared_rel_tol
-fitModel <- function(pulseData,
-                     params,
-                     shared_params = NULL,
-                     size=100,
-                     options = list()) {
+fitModel <- function(pulseData, par, options = list()) {
   require(parallel)
   opts <- defaultParams()
   param_names <- names(params)
   opts$parscales <- mapply(max,
-                           abs(options$upper_boundary),
-                           abs(options$lower_boundary))
+    abs(options$upper_boundary),
+    abs(options$lower_boundary))
   rel_err <- 10 * opts$rel_tol
   if (is.null(shared_params)) {
     shared_rel_err <- 0
@@ -113,43 +83,19 @@ fitModel <- function(pulseData,
          shared_rel_err > opts$shared_rel_tol) {
     # Fit params for every genes individually
     log2screen(opts, "Fitting gene-specific params\n")
-    old_params <- params
-    params <- fitIndividualParameters(
-      old_params = old_params,
-      pulseData = pulseData,
-      shared_params = shared_params,
-      options = opts,
-      size = size
-    )
-    rel_err <- getMaxRelDifference(params, old_params)
+    params <- fitIndividualParameters(pulseData, par, opts)
+    rel_err <- getMaxRelDifference(params, par$individual_params)
+    par$individual_params <- params
     # Fit shared params
-    if (!is.null(shared_params)) {
+    if (!is.null(par$shared_params)) {
       log2screen(opts, "Fitting shared params\n")
-      old_shared_params <- shared_params
-      shared_params <- fitSharedParameters(
-        old_shared_params = shared_params,
-        pulseData = pulseData,
-        individual_params = params,
-        options           = opts,
-        size              = size
-      )
-      shared_rel_err <-
-        getMaxRelDifference(shared_params, old_shared_params)
+      shared_params <- fitSharedParameters(pulseData, par, opts)
+      shared_rel_err <- getMaxRelDifference(shared_params, par$shared_params)
+      par$shared_params <- shared_params
       log2screen(opts, "Shared params\n")
-      log2screen(opts, toString(shared_params), "\n")
+      log2screen(opts, toString(par$shared_params), "\n")
     }
-    size <- fitDispersion(
-      shared_params = shared_params,
-      pulseData = pulseData,
-      individual_params = params,
-      options = opts,
-      size = size
-    )
+    par$size <- fitDispersion(pulseData, par, opts)
   }
-  list(
-    individual_params = params,
-    shared_params = shared_params,
-    size = size, 
-    formulas = pulseData$formulas
-  )
+  list(par=par, formulas = pulseData$formulas)
 }
