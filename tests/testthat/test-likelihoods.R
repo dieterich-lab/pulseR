b62bf21582ab2f7ca01d5a684b54b979f62933ab
alexey0308 <alexey.mipt@gmail.com>
2016-12-28 16:42:51 +0100

    first testthat test: contaminate

---
+++ tests/testthat/test-likelihoods.R
@@ -0,0 +1,7 @@
+context("likelihood functions and formula manipulations")
+
+test_that("correct fraction contamination",{
+  formulas <- MeanFormulas(label=a, unlabel=b, extra=d + 1)
+  expected <- quote((1-p)*(a) + p*(b))
+  expect_equal(contaminate(formulas, "label", "unlabel", "p"), expected)
+})
