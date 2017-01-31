45a317c2883c50dbbe3dc3cbb3e9eaa8de78c99d
Alexey U <alexey.mipt@gmail.com>
2017-01-31 23:06:11 +0100

    grow_

--- R/formulas.R
+++ R/formulas.R
@@ -9,3 +9,10 @@ degrade_ <- function(x, d, t){
 degrade <- function(x, d, t){
 	substitute( x * exp(-d*t))
 }
+
+grow_ <- function(x, mu, d, t){
+	substitute(mu - (mu - x) * exp(-d*t), 
+		list(x=as.name(x),
+				 d=as.name(d),
+				 t=as.name(t)))
+}
