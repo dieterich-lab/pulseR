64610b100b182bd4f57db3673e757dd7b10fe44b
alexey0308 <alexey.mipt@gmail.com>
2017-02-14 13:50:15 +0100

    tests

--- tests/testthat/test-fitModel-fractions.R
+++ tests/testthat/test-fitModel-fractions.R
@@ -6,17 +6,13 @@ nGenes <- 40
 nReplicates <- 3
 nTime <- 4
 
-options <- setBoundaries(
-  params = list(lb = c(1, 1e-3),
-                up = c(1e10, 1-1e-3)),
-  fraction_factors = list(lb = .1, ub = 10)
+formulas <- MeanFormulas(A = a,
+                         B =  a * b ^ time,
+                         C = a * (1 - b ^ time))
+conditions <- data.frame(
+  condition = rep(names(formulas), each = nTime),
+  time = rep(1:nTime, length(formulas) * nReplicates)
 )
-
-options$cores <- 1
-
-formulas <- MeanFormulas(A = a, B =  a * b^time, C= a * (1-b^time))
-conditions <- data.frame(condition = rep(names(formulas), each = nTime),
-                         time = rep(1:nTime, length(formulas) * nReplicates))
 rownames(conditions) <- paste0("sample_", seq_along(conditions$condition))
 t <- pulseR:::addKnownShared(formulas, conditions)
 formulas_known <- t$formulas
@@ -24,8 +20,8 @@ conditions_known <- data.frame(condition = t$conditions)
 
 par <- list(size = 1e2)
 fractions <- factor(conditions_known$condition)
-par$params <-
-  data.frame(a = (1:nGenes) * 1e5, b = runif( nGenes,.1,.8))
+par$params <- data.frame(a = (1:nGenes) * 1e5,
+                         b = runif(nGenes, .1, .8))
 rownames(par$params) <- paste0("gene_", 1:nGenes)
 
 par$fraction_factors <- 1 * (1:(length(levels(fractions)) ))
@@ -40,8 +36,12 @@ pd <- PulseData(
     formulas   = formulas,
     fractions  = ~condition+time)
 
+options <- setBoundaries(
+  params = list(a = c(1,1e10), b = c(.01,1-.01)),
+  fraction_factors = c(0.1,10))
+
 test_that("all together fitting works", {
-  skip("...")
+  #skip("...")
   par2 <- par
   guess <-  apply(counts[, conditions$condition == "A"], 1, mean)
   par2$params$a <- guess
