0420313af8216ba50c84dd41630160fd3e94a86d
alexey0308 <alexey.mipt@gmail.com>
2017-02-21 15:45:20 +0100

    working prototype with fraction list

--- R/fractions.R
+++ R/fractions.R
@@ -4,28 +4,33 @@ forms <- alist(
   unlab = b,
   total = b + a)
   
-counts <- data.frame(
-  lab=1:10, 
-  unlab=runif(10)*10,
-  total = abs(rnorm(10)) +1:10)
-  
-par <- data.frame(a=1:10, b=10:1)
+nGenes <- 7
+## params are just env
+par <- list(size = 1e2)
+par$alpha <-  1
+par <-  c(par, list(
+  a = (1:nGenes) * 1e5, b = rep(1e6, nGenes)))
+par$size <- 100000
 
-form_indexes <- list(
+
+counts <- as.matrix(data.frame(
+  lab = rnbinom(nGenes, size = par$size, mu = par$a),
+  unlab = rnbinom(nGenes, size = par$size, mu = par$b),
+  total = rnbinom(nGenes, size = par$size, mu = par$a + par$b)
+))
+  
+
+formIndexes <- list(
   c(1,2),
   c(1,2),
   3
 )
 
-norm_factors <- list(
-  c(5,6),
-  c(2,7),
+normFactors <- list(
+  c(1,0),
+  c(0,1),
   1
 )
-
-llgene <- function(params){
-  
-}
 
 evaled <- lapply(forms, eval, par)
 
@@ -40,11 +45,52 @@ sample_means <- function(evaled_forms, form_indexes, norm_factors){
   do.call(cbind, mus)
 }
 
-ll <- function(mus, counts, size){
-  sum(dnbinom(counts, mus, log=TRUE, size=size))
+ll <- function(counts, par, namesToOptimise) {
+  p <- par[namesToOptimise]
+  par[namesToOptimise] <- NULL
+  function(x){
+    par[namesToOptimise] <- relist(x, p)
+    evaledForms <- lapply(forms, eval, envir = par) 
+    means <- sample_means(evaledForms, formIndexes, normFactors)
+    -sum(dnbinom(counts, mu = means, size = par$size, log = TRUE))
+  }
 }
 
-.mu <- function(evaled_form_list, norm_fac){
-  
+f <- ll(counts, par, c("a", "b"))
+
+paramClasses <- list(
+  part = c("a", "b"),
+  shared = c("alpha"))
+
+# extend boundaries to param length
+.b <- function(b, par) {
+  for (p in names(b)) {
+    if (length(b[[p]]) == 1)
+      b[[p]] <- rep(b[[p]], length(par[[p]]))
+  }
+  b
 }
 
+fitParams <- function(counts, par, namesToOptimise, opts){
+ objective <- ll(counts, par, namesToOptimise)
+ lb <- unlist(opts$lb[namesToOptimise])
+ ub <- unlist(opts$ub[namesToOptimise])
+ x <- unlist(par[namesToOptimise])
+ x <- optim(
+   x,
+   objective,
+   method = "L-BFGS-B",
+   control = list(parscale = x),
+   lower = lb,
+   upper = ub
+ )$par
+ relist(x, par[namesToOptimise])
+}
+
+opts <- list()
+opts$lb <- list(a=rep(.1, b=.1))
+opts$lb <- .b(opts$lb, par)
+opts$ub <- list(a=1e7, b=2e7)
+opts$ub <- .b(opts$ub, par)
+                
+fitParams(counts, par, c("a", "b"), opts)
