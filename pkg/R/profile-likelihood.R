ccbaac18d6561518ba360819019ef8b09737fc79
Alexey U <alexey.mipt@gmail.com>
2017-06-28 17:59:31 +0200

    add error catch

--- pkg/R/profile-likelihood.R
+++ pkg/R/profile-likelihood.R
@@ -1,25 +1,26 @@
-result
-
-getIndex <- function(path, example) {
+.getIndex <- function(path, example) {
   x <- unlist(example)
   x[] <- seq_along(x)
-  
+  index <- .getElement(relist(x, example), path)
+  if (length(index) > 1) 
+    stop("Not complete path to the element (length of selection > 1)")
+  index
 }
   
 .assignElement <- function(x, i, value) {
   if (length(i) == 1) {
-    x[[i]] <- value 
+    x[[unlist(i)]] <- value 
     x
   } else {
-    x[[i[1]]] <- Recall(x[[i[1]]], i[-1], value)
+    x[[unlist(i[1])]] <- Recall(x[[unlist(i[1])]], i[-1], value)
     x 
   }
 }
 
 .getElement <- function(x, i) {
   if (length(i) == 1) {
-    x[[i]] 
+    x[[unlist(i)]] 
   } else {
-    Recall(x[[i[1]]], i[-1])
+    Recall(x[[unlist(i[1])]], i[-1])
   }
 }
