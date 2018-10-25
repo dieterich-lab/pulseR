fdcd3e392c99a809902aa4f7d0c00acea7f7b6c4
alexey0308 <alexey.mipt@gmail.com>
2018-10-25 11:14:06 +0200

    style

--- pkg/R/fit.R
+++ pkg/R/fit.R
@@ -1,61 +1,61 @@
 
 
-#' Fit model parameters 
+#' Fit model parameters
 #'
 #' `fitModel` is the main fitting function which wraps around the others and
 #' fit iteratively the whole model. \cr
-#' `fitParams` fits only the listed parameters and `fitParamsSeparately` fit 
+#' `fitParams` fits only the listed parameters and `fitParamsSeparately` fit
 #' gene-specific parameters having the shared parameters (e.g. normalisation
-#' factors). \cr 
-#' `fitNormFactors` fits the normalisation factors having fixed all other 
+#' factors). \cr
+#' `fitNormFactors` fits the normalisation factors having fixed all other
 #' parameters.
-#' 
+#'
 #' @param pulseData the \code{\link{PulseData}} object
 #' @param par a list with an initial parameters values.
-#' Names correspond to the parameter names used in formulas. 
+#' Names correspond to the parameter names used in formulas.
 #' `size` corresponds to the size parameter, `normFactors` stands for the
 #' list with normalistion factors (in spike-in free design, see details).
 #' There are following parameter types:
-#'   - gene-specific parameters must be set as vectors of the length equal to 
-#'     the gene number. The function \link{initParameters} may 
+#'   - gene-specific parameters must be set as vectors of the length equal to
+#'     the gene number. The function \link{initParameters} may
 #'     simplify the process of initial values randomisation.
 #'   - shared parameters. These are prersented by single numeric values,
-#'     which are assumed to be equal between all genes, e.g. additional 
+#'     which are assumed to be equal between all genes, e.g. additional
 #'     normalisation fator.
-#'   - the size parameter for the negative binomial 
-#'     distribution and a list with the normalisation factors, if 
+#'   - the size parameter for the negative binomial
+#'     distribution and a list with the normalisation factors, if
 #'     no spike-ins are used in the experiment.
 #'   - normalisation factors (in case of spike-in free design).
-#'     
-#' @param namesToOptimise a vector of names of parameters, which values 
+#'
+#' @param namesToOptimise a vector of names of parameters, which values
 #'   need be optimised
 #' @param knownGenePars a vectors of names of the gene-specific parameters,
 #'  which  are assumed to be fixed during optimisation.
 #' @param indexes indexes of genes to fit. By default includes all the genes.
 #' @param options a list of options. For more details, see \link{setBoundaries},
 #'   \link{setTolerance}, \link{setFittingOptions}
-#' 
+#'
 #' @details If no spike-ins are used, relations between samples are inferred
-#' during the model fitting. In this case, the initial parameter list must 
-#' containg  a field named `normFactors`. The normalistion factors are 
+#' during the model fitting. In this case, the initial parameter list must
+#' containg  a field named `normFactors`. The normalistion factors are
 #' accepted as a named list, e.g.
 #' ```
 #' par$normFactors <- list(total_fraction = 1,
 #'       pull_down.4 = c(1, 0.01),
 #'       pull_down.8 = c(1, 0.01))
 #' ```
-#' This will define the initial values for the normalisation factors. 
+#' This will define the initial values for the normalisation factors.
 #' The very first value is **always** equal 1 irregardless of the user input.
 #' This has to be done because the normalisation factors are known
-#' only up to some scaling coefficient, because they appear in 
+#' only up to some scaling coefficient, because they appear in
 #' a multiplication with the expression level or synthesis rate.
-#' 
-#' The structure of the `normFactors` list is identical to the 
-#' `pulseData$interSampleCoeffs`. This structure is defined by the 
+#'
+#' The structure of the `normFactors` list is identical to the
+#' `pulseData$interSampleCoeffs`. This structure is defined by the
 #' `formulaIndexes` and `conditions` argumenta in the `PulseData`,
 #'  see `\link{PulseData}` for more.
-#' 
-#' `\link{fitParamsSeparately}` is same as \link{fitParams}, 
+#'
+#' `\link{fitParamsSeparately}` is same as \link{fitParams},
 #' but performs optimisation for gene-specific parameters only.
 #' Every set of parameters is fitted individually for every gene.
 #'
@@ -89,7 +89,7 @@ fitParamsSeparately <- function(pulseData,
                                 par,
                                 knownGenePars,
                                 namesToOptimise,
-                                options, 
+                                options,
                                 indexes = seq_len(dim(pulseData$counts)[1])) {
   if (missing(knownGenePars))
     knownGenePars <- character(0)
@@ -154,7 +154,7 @@ fitModel <- function(pulseData, par, options){
   log2screen(options, cat("\n"))
   # identify what to fit and what is fixed
   known   <- .getKnownNames(par, options)
-  knownGenePars <- .getKnownGeneNames(par, known) 
+  knownGenePars <- .getKnownGeneNames(par, known)
   fitSets <- list(
     params = .getGeneToFitNames(par, known),
     shared = .getSharedNames(par, known),
@@ -168,14 +168,14 @@ fitModel <- function(pulseData, par, options){
   options <- normaliseBoundaries(
     options, par[setdiff(names(par), known)], pulseData)
   funs <- list(
-    params = function(par) 
+    params = function(par)
       fitParamsSeparately(pulseData, par, knownGenePars, fitSets$params, options),
     shared = function(par)
       fitParams(pulseData, par, fitSets$shared, options),
-    normFactors = function(par) 
+    normFactors = function(par)
       list(normFactors = fitNormFactors(pulseData, par, options))
   )
-  
+
   err <- c(params = Inf, shared = Inf, normFactors = Inf)[names(fitSets)]
   err <- c(err, logLik = Inf)
   logLik <- -Inf
@@ -216,11 +216,11 @@ progressString <- function(err, logLik) {
                 digits = 2,
                 width = 6)
   logLik <- format(logLik, digits=2, width=6)
-  paste0("LogLik: [", logLik, " ] Max Rel.err. in ", 
+  paste0("LogLik: [", logLik, " ] Max Rel.err. in ",
          paste("[",names(str), str,"]", collapse = " "), "\n")
 }
 
-# if a parameter is not mentioned in the boundaries, 
+# if a parameter is not mentioned in the boundaries,
 # it is assumed to be fixed
 .getKnownNames <- function(par, options) {
   setdiff(names(par), names(options$lb))
@@ -253,6 +253,6 @@ progressString <- function(err, logLik) {
 .getSharedNames <- function(par, known) {
   toFit <- .namesToFit(par, known)
   len <- vapply(par, length, integer(1))
-  sharedParams <- toFit[len[toFit] == 1] 
+  sharedParams <- toFit[len[toFit] == 1]
   sharedParams
 }
